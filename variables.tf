d2f0cc502d2bc42110f85ca5d04aa76646c73073
Sandro William Pereira <sandrowp@hotmail.com>
2023-01-29 21:47:27 -0300

    Carga inicial

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "network_name" {
+  description = "Nome da Rede"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "centro_custo_rh" {
+  description = "Nome da Rede"
+  type        = string
+  default     = "rh"
+}
+
+variable "project_id" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "innate-agency-369817"
+}
